0aa9865abb4dbf8353c1f477adf6331a1db240af
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-10-23 12:06:49 -0500

    Updates

---
+++ precache-manifest.7731c617f558a4714fd15e0f52fc5b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3576d94e8c46a41dd9c0",
+    "url": "/lights-out-reactapp/static/css/main.f08f0eb5.chunk.css"
+  },
+  {
+    "revision": "3576d94e8c46a41dd9c0",
+    "url": "/lights-out-reactapp/static/js/main.3576d94e.chunk.js"
+  },
+  {
+    "revision": "024dd0e473331f56a349",
+    "url": "/lights-out-reactapp/static/js/1.024dd0e4.chunk.js"
+  },
+  {
+    "revision": "186be2400f08bca2430c",
+    "url": "/lights-out-reactapp/static/js/runtime~main.186be240.js"
+  },
+  {
+    "revision": "2b4138022c97366ffb14a1c17b83fef0",
+    "url": "/lights-out-reactapp/index.html"
+  }
+];
